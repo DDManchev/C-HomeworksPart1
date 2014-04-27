fe7175039e36193c73a5a724d3b9ae686236afa5
DDManchev <d.d.Manchev@gmail.com>
2014-04-27 22:37:19 +0300

    Task from 2.4.14 and ex10 from Loops chapter

---
+++ Loops/ex10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+ 
+class ex10
+{
+    static void Main()
+    {
+        // Catalan formula =  (2n)! / (n + 1)! * n!
+ 
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        int totalSum;
+        decimal product2n = 1;
+        decimal productn = 1;
+        decimal product_n_1 = 1;
+ 
+        for (int i = 1; i <= 2*n; i++)
+        {
+           product2n *= i;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            productn *= i;
+        }
+        for (int i = 1; i <= n + 1; i++)
+        {
+            product_n_1 *= i;
+        }
+        totalSum = product2n / (productn * product_n_1);
+            Console.WriteLine("(2n)! / (n + 1)! * n! = {0}", product);
+    }
+}
+   
---
+++ Task - 2.4.14.cs
@@ -0,0 +1,44 @@
+﻿using System;
+ 
+class SubSetSum
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the wanted subset sum");
+        int wantedSum = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of elements:");
+        int numels = int.Parse(Console.ReadLine());
+        int[] elements = new int[numels];
+        int counter = 0;
+        string subset = "";
+        for (int i = 0; i < elements.Length; i++)
+        {
+            Console.WriteLine("Enter element Number: {0}", i + 1);
+            elements[i] = int.Parse(Console.ReadLine());
+        }
+        int maxSubsets = (int)Math.Pow(2, numels) - 1;
+        for (int i = 1; i <= maxSubsets; i++)
+        {
+            subset = "";
+            int checkingSum = 0;
+            for (int j = 0; j <= numels; j++)
+            {
+                int mask = 1 << j;
+                int nAndMask = i & mask;
+                int bit = nAndMask >> j;
+                if (bit == 1)
+                {
+                    checkingSum = checkingSum + elements[j];
+                    subset = subset + " " + elements[j];
+                }
+            }
+            if (checkingSum == wantedSum)
+            {
+                Console.WriteLine("Number of subest that have the sum of {0}", wantedSum);
+                counter++;
+                Console.WriteLine("This subset has a sum of {0} : {1} ", wantedSum, subset);
+            }
+        }
+        Console.WriteLine(counter);
+    }
+}
